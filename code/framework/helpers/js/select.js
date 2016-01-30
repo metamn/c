55a9cbe3b8172b40091c6f47af0497e842945fd0
cs <bartus.csongor@gmail.com>
2016-01-30 02:14:08 +0200

    Working on the slider

--- code/framework/helpers/js/select.js
+++ code/framework/helpers/js/select.js
@@ -11,8 +11,12 @@ var select = function(IDs, container) {
 
 // Loop
 //
-// This is a replacement for the for() loop
+// This is a replacement for the `for()` loop
 // - https://css-tricks.com/snippets/javascript/loop-queryselectorall-matches/
+//
+// It is important to avoid getting array elements by index, like `figures[i]`
+// - with `loop` if the `figures` are empty we won't get any error messages;
+// - with a `for` loop we will get an error and everything will halt
 //
 NodeList.prototype.loop = Array.prototype.forEach;
 
