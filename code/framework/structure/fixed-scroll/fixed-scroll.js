57d44b6852b6f0b78dac45b626af44733af8d312
metamn <bartus.csongor@gmail.com>
2016-04-18 16:19:40 +0300

    Fixed slide done

--- code/framework/structure/fixed-scroll/fixed-scroll.js
+++ code/framework/structure/fixed-scroll/fixed-scroll.js
@@ -14,11 +14,34 @@ var fixedScrolling = function() {
   var slidesSelector = slidesContainerID + ' ' + slideID;
   var slides = select(slidesSelector);
 
-  console.log('throttle for ' + slidesSelector + ' ' + slides.length);
+  var slidesContainer = select(slidesContainerID);
+  var slidesContainerHeight = slidesContainer[0].offsetHeight;
+  var slideHeight = slidesContainerHeight / slides.length;
 
+  var activeSlide = getActiveSlide(slideHeight, slides);
 
   klass(slides, 'fixed-scroll__slide--active', 'removeAll');
-  klass(slides[0], 'fixed-scroll__slide--active', 'add');
+  klass(activeSlide, 'fixed-scroll__slide--active', 'add');
 };
+
+function getActiveSlide(slideHeight, slides) {
+  var activeSlide = 0;
+
+  for (var i = 0; i < slides.length; i++) {
+    if (isScrolledIntoView(slideHeight, slides[i])) {
+      activeSlide = i;
+      break;
+    }
+  }
+
+  return slides[activeSlide];
+}
+
+// http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
+function isScrolledIntoView(h, el) {
+  var elemBottom = el.getBoundingClientRect().bottom;
+  var isVisible = (elemBottom >= 0) && (elemBottom <= h);
+  return isVisible;
+}
 
 module.exports = fixedScroll;
