989f20b08e85cca66114f65562f11d448703a049
metamn <bartus.csongor@gmail.com>
2016-04-18 15:30:41 +0300

    Ready to add the active slide check

--- code/framework/structure/fixed-scroll/fixed-scroll.js
+++ code/framework/structure/fixed-scroll/fixed-scroll.js
@@ -1,4 +1,7 @@
 var _ = require('lodash');
+var l = require('./../../helpers/js/loop.js');
+var select = require('./../../helpers/js/select.js');
+var klass = require('./../../helpers/js/klass.js');
 
 var fixedScroll = function(slidesContainerID, slideID) {
   window.addEventListener('scroll', _.throttle(_.bind(fixedScrolling, this, slidesContainerID, slideID), 300));
@@ -7,9 +10,15 @@ var fixedScroll = function(slidesContainerID, slideID) {
 var fixedScrolling = function() {
   var slidesContainerID = arguments[0];
   var slideID = arguments[1];
-  
+
   var slidesSelector = slidesContainerID + ' ' + slideID;
-  console.log('throttle for ' + slidesSelector);
+  var slides = select(slidesSelector);
+
+  console.log('throttle for ' + slidesSelector + ' ' + slides.length);
+
+
+  klass(slides, 'fixed-scroll__slide--active', 'removeAll');
+  klass(slides[0], 'fixed-scroll__slide--active', 'add');
 };
 
 module.exports = fixedScroll;
--- code/pages/home/home.js
+++ code/pages/home/home.js
@@ -1,2 +1,2 @@
 var fixedScroll = require('./../../framework/structure/fixed-scroll/fixed-scroll.js');
-fixedScroll('x-fixed-scroll', '.slide');
+fixedScroll('.x-fixed-scroll', '.fixed-scroll__slide');
